f09c8a279d192c5fb15c7f6fd406ff3cd9366510
pelin <kayhanpelin@gmail.com>
2019-04-30 14:40:30 +0100

    Updates

---
+++ precache-manifest.540ad88599e252c35db0997fd5cc1141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df96e9a7ccdbf9a095e6d8f459855d7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f54879c812227a849dc",
+    "url": "/portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2f54879c812227a849dc",
+    "url": "/portfolio/static/js/main.0084484b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
